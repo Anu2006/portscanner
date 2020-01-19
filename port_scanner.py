4cf942505a226f692f395a15705a75b551ae5940
Anu2006 <55542548+Anu2006@users.noreply.github.com>
2020-01-19 10:11:10 +0530

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -2,7 +2,6 @@ from threading import Thread
 from colorama import Fore
 import socket
 
-
 host = '192.168.0.1'
 ports = 1024
 
@@ -10,10 +9,11 @@ ports = 1024
 def port_scanner(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if sock.connect_ex((host, port)):
-        return
+        print(Fore.RED + f'\nport {port} is closed', end='')
+        # return
 
     else:
-        print(Fore.GREEN + f'port {port} is opened')
+        print(Fore.GREEN + f'\nport {port} is opened', end='')
 
 
 def main():
