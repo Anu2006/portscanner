334d7906f3e1dc773c164603ea2057544e3c828f
Anu2006 <55542548+Anu2006@users.noreply.github.com>
2020-01-19 07:11:11 +0530

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,42 +1,26 @@
 from threading import Thread
 from colorama import Fore
-import asyncio
 import socket
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-# socket.setdefaulttimeout(1)
 
-host = '192.168.1.4'
-num_of_ports = 1024
-port = 445
+host = '192.168.0.1'
+ports = 1024
 
-async def port_scanner(port):
-    try:
-        sock.connect(port)
-        return True
 
-    except:
-        return False
-    # if sock.connect_ex((host, port)):
-        # return Fore.RED + f'Port {port} of host {host} is closed'
-    #
-    # else:
-    #     return Fore.GREEN + f'Port {port} of host {host} is opened'
+def port_scanner(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if sock.connect_ex((host, port)):
+        return
 
-async def main():
-    for port in range(num_of_ports):
-        res = await port_scanner(139)
-        if res:
-            print(Fore.GREEN + f'Port {port} of host {host} is opened')
+    else:
+        print(Fore.GREEN + f'port {port} is opened')
 
-        else:
-            print(Fore.RED + f'Port {port} of host {host} is closed')
 
-        print(Fore.WHITE, end='')
-        # t = Thread(target=port_scanner, args=(i,))
-        # t.start()
+def main():
+    for port in range(1, ports + 1):
+        t = Thread(target=port_scanner, args=(port,))
+        t.start()
+
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+    main()
